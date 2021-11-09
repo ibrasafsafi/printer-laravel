b649de37d480ea3bad1c9fcc01347ee43d058d87
safsafi <safsafi820@gmail.com>
2021-11-09 12:58:51 +0000

    first commit

---
+++ src/providers/PrinterServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Ibrasafsafi\PrinterLaravel\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PrinterServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        dd("OOOOOKKKKKKKKKK");
+    }
+}
